59e3ecf4a40949e08956d1d19752ecd38f0d3a97
Ethan-Yuanxy <947253705@qq.com>
2022-05-04 21:27:12 +0800

    less环境配置

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import less from 'less'
+Vue.use(less)
+
 //全局样式
-// import './css/common.less'
+import './css/common.less'
 
 Vue.config.productionTip = false
 
